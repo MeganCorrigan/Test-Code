0b6dd30f06d6291b22e395f928bd1b0712ce4ce0
MeganCorrigan <73315164+MeganCorrigan@users.noreply.github.com>
2021-06-03 11:53:34 -0700

    Black Screen pokemon game

--- Pokemon.py
+++ Pokemon.py
@@ -1,16 +1,12 @@
 import pygame
 import Configurations
 
-from game import Game
-
 pygame.init()
 
-screen = pygame.display.set_mode(600, 400)
+screen = pygame.display.set_mode((600, 400))
 
 pygame.display.set_caption("Pokemon Clone")
 
-game = Game(screen)
-
 while True:
-    screen.fill(config.BLACK)
+    screen.fill(Configurations.BLACK)
     pygame.display.flip()
